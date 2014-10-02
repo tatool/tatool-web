2f44f41abfa5f8d4fc45158ff53b9dd05f2d2e68
alocher <jimmy@outerlimits.ch>
2014-10-02 14:17:09 -0600

    fixed issue with feedback panel not working properly

--- app/scripts/module/statuspanel.service.js
+++ app/scripts/module/statuspanel.service.js
@@ -25,7 +25,7 @@ angular.module('tatool.module')
 
   // feedback panel
   service.updateFeedback = function(feedback) {
-    if (!feedback) {
+    if (feedback == undefined) {
       service.feedback = null;
     } else if (feedback === 1) {
       service.feedback = 'correct';
