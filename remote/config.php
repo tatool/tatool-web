cf5b4741cf2a4a34290036e27423cbe20a9cc817
alocher <jimmy@outerlimits.ch>
2015-06-26 18:15:35 -0600

    typo

--- remote/config.php
+++ remote/config.php
@@ -2,7 +2,7 @@
 # path to the utility php file LZString.php
 require("../LZString.php");
 
-# path to your tatool-web data folder including a trailing slash (the folder where your use data should be uploaded)
+# path to your tatool-web data folder including a trailing slash (the folder where your user data should be uploaded)
 $tatoolwebpath = "/home/outerlim/tatoolweb/";
 
 ?>
