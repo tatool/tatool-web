9dd387e2c499205ab53a8a7159d7cdbb96b2ca64
cvonbastian <claudia@outerlimits.ch>
2015-11-29 15:41:08 -0700

    Memory Span Training Task
    Training version of tatoolMemorySpan for review

---
+++ app/projects/public/tatool/executables/tatoolMemorySpanTraining.ctrl.js
@@ -0,0 +1,99 @@
+'use strict';
+
+tatool.controller('memorySpanTrainingCtrl', [ '$scope', 'service', 
+    function ($scope, service) {
+
+    $scope.stimulusService = service.stimulusService;
+    $scope.inputService = service.inputService;
+
+    $scope.start = function() {
+      service.inputService.hide();
+      service.inputService.disable();
+
+      switch (service.getPhase()) {
+        case 'INIT':
+          service.createStimulus();
+          service.setPhase('MEMORISATION');
+          memorisationPhase();
+          break;
+        case 'MEMORISATION':
+          memorisationPhase();
+          break;
+        case 'RECALL':
+          recallPhase();
+          break;
+      }
+    };
+
+    // Display memoranda on screen for a given amount of time
+    function memorisationPhase() {
+      // increment response counter
+      service.memCounter++;
+
+      // set phase to recall if this is the last memoranda
+      if (service.memCounter === service.stimulus.stimulusCount) {
+        service.setPhase('RECALL');
+      }
+
+      // set memoranda
+      service.setStimulus();
+
+      // start timer and show memoranda
+      if (service.timerEnabled.propertyValue === true) {
+        service.timerDisplayMemoranda.start(memorisationTimeUp);
+      }
+      service.stimulusService.show();
+    }
+
+    // Remove memoranda from screen and display next or stop executable
+    function memorisationTimeUp() {
+      service.stimulusService.hide();
+      if (service.suspendAfterEachItem.propertyValue) {
+        // suspend after each item
+        service.suspendExecution();
+      } else {
+        if (service.getPhase() == 'MEMORISATION') {
+          if (service.timerEnabled.propertyValue === true) {
+            service.timerIntervalMemoranda.start(memorisationPhase);
+          }
+        } else {
+          // suspend after all items
+          service.suspendExecution();
+        }
+      }
+    }
+
+    // Display question to recall memoranda
+    function recallPhase() {
+      // increment response counter
+      service.respCounter++;
+
+      var stimulusText = service.recallText + ' ' + service.respCounter + ' ?';
+      service.setRecallStimulus(stimulusText);
+
+      service.startTime = service.stimulusService.show();      
+      service.inputService.show();
+      service.inputService.enable();
+    }
+
+    // Capture user input
+    $scope.inputAction = function(input, timing, event) {
+      service.inputService.disable();
+      service.inputService.hide();
+      service.stimulusService.hide();
+
+      service.endTime = timing;
+      processResponse(input.givenResponse);
+    };
+
+    // Provide service with the response and time of response
+    function processResponse(givenResponse) {
+      if (service.respCounter < service.stimulus.stimulusCount) {
+        service.addTrial(givenResponse).then(recallPhase);
+      } else {
+        service.setPhase('INIT');
+        service.addTrial(givenResponse).then(service.stopExecution);
+      }
+    }
+
+  }]);
---
+++ app/projects/public/tatool/executables/tatoolMemorySpanTraining.service.js
@@ -0,0 +1,135 @@
+'use strict';
+
+tatool.factory('memorySpanTraining', ['executableUtils', 'dbUtils', 'timerUtils', 'stimulusServiceFactory', 'inputServiceFactory',
+  function(executableUtils, dbUtils, timerUtils, stimulusServiceFactory, inputServiceFactory) {
+
+    var MemorySpanTraining = executableUtils.createExecutable();
+
+    var DISPLAY_DURATION_DEFAULT = 800;
+    var INTERVAL_DURATION_DEFAULT = 400;
+    var RECALL_TEXT_DEFAULT = 'Stimulus';
+
+    MemorySpanTraining.prototype.init = function() {
+      this.phase = 'INIT';
+
+      if (!this.suspendAfterEachItem) {
+        this.suspendAfterEachItem = {
+          propertyValue: true
+        };
+      }
+
+      if (!this.timerEnabled) {
+        this.timerEnabled = {
+          propertyValue: true
+        };
+      } else {
+        this.timerEnabled.propertyValue = (this.timerEnabled.propertyValue === true) ? true : false;
+      }
+
+      // template properties
+      this.recallText = (this.recallText) ? this.recallText : RECALL_TEXT_DEFAULT;
+      this.stimulusService = stimulusServiceFactory.createService();
+      this.inputService = inputServiceFactory.createService();
+
+      // timing properties
+      this.displayDuration = (this.displayDuration) ? this.displayDuration : DISPLAY_DURATION_DEFAULT;
+      this.intervalDuration = (this.intervalDuration) ? this.intervalDuration : INTERVAL_DURATION_DEFAULT;
+      this.timerDisplayMemoranda = timerUtils.createTimer(this.displayDuration, true, this);
+      this.timerIntervalMemoranda = timerUtils.createTimer(this.intervalDuration, false, this);
+      
+      var self = this;
+
+    };
+
+    // Create stimulus and set properties
+    MemorySpanTraining.prototype.createStimulus = function() {
+      // reset executable properties
+      this.startTime = 0;
+      this.endTime = 0;
+      this.digits = [];
+      this.memCounter = 0;
+      this.respCounter = 0;
+
+      var numbers = [];
+
+      // fill numbers array with two-digit numbers from 11 to 99
+      for (var i = 11; i < 100; i++) {
+        numbers[i - 11] = i;
+      }
+
+      var levelHandler = dbUtils.getHandler('complexSpanLevel');
+      var currentLevel = dbUtils.getModuleProperty(levelHandler, 'currentLevel');
+
+      if (currentLevel == null) {
+        currentLevel = 1;
+      }
+
+      this.stimulus = new Array();
+
+      // draw as many numbers as level+1
+      var nStimuli = currentLevel + 1;
+
+      // create stimulus properties
+      this.stimulus["stimulusCount"] = nStimuli;
+
+      for (var j = 1; j < nStimuli + 1; j++) {
+        this.stimulus['stimulusValueType' + j] = 'text';
+        this.stimulus['stimulusValue' + j] = executableUtils.getRandom(numbers);
+        this.stimulus['correctResponse' + j] = this.stimulus['stimulusValue' + j];
+      }
+
+      for (var index in this.stimulus) {
+        console.log(index + " : " + this.stimulus[index]);
+      }
+    };
+
+    MemorySpanTraining.prototype.setStimulus = function() {
+      this.stimulusService.set({
+        stimulusValueType: this.stimulus['stimulusValueType' + this.memCounter],
+        stimulusValue: this.stimulus['stimulusValue' + this.memCounter]
+      });
+    };
+
+    MemorySpanTraining.prototype.setRecallStimulus = function(text) {
+      this.stimulusService.setText({
+        stimulusValue: text
+      });
+    };
+
+    MemorySpanTraining.prototype.getPhase = function() {
+      return this.phase;
+    };
+
+    MemorySpanTraining.prototype.setPhase = function(phase) {
+      this.phase = phase;
+    };
+
+    // Process given response and stop executable
+    MemorySpanTraining.prototype.addTrial = function(givenResponse) {
+      this.trial = {};
+      this.trial.setSize = this.stimulus.stimulusCount;
+      this.trial.reactionTime = this.endTime - this.startTime;
+      this.trial.givenResponse = givenResponse;
+      this.trial.correctResponse = this.stimulus['correctResponse' + this.respCounter];
+
+      if (this.trial.correctResponse.toString().toLowerCase() == this.trial.givenResponse.toLowerCase()) {
+        this.trial.score = 1;
+      } else {
+        this.trial.score = 0;
+      }
+
+      return dbUtils.saveTrial(this.trial);
+    };
+
+    MemorySpanTraining.prototype.stopExecution = function() {
+      executableUtils.stop();
+    };
+
+    MemorySpanTraining.prototype.suspendExecution = function() {
+      executableUtils.suspend();
+    };
+
+    return MemorySpanTraining;
+
+  }
+]);
