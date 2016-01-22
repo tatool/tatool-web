56b0fa6e8d109b2a044c324840e6d120eb2fdbd4
Laurent Schüpbach <laurent.schuepbach@protonmail.ch>
2016-01-22 14:11:01 +0100

    Chimeric Face Task added
    TODO:
    - Make it able to show two images
    - Add the right stimuli

---
+++ app/projects/public/Representation Assymetry/executables/chimeric.ctrl.js
@@ -0,0 +1,35 @@
+tatool.controller('chimericCtrl', [ '$scope', 'service',
+  function ($scope, service) {
+
+    $scope.stimulusService = service.stimulusService;
+    $scope.inputService = service.inputService;
+
+    $scope.start = function() {
+      service.createStimulus();
+
+      service.inputService.show();
+      service.inputService.enable();
+
+      //Start the visual timer
+      service.timer.start(timerUp);
+
+      //Start the real timing
+      service.startTime = service.stimulusService.show();
+
+    };
+
+    // Called by timer when time elapsed without user input
+    function timerUp() {
+      service.inputService.disable();
+      service.endTime = service.stimulusService.hide();
+      service.processResponse('');
+    }
+
+    $scope.inputAction = function(input, timing, event) {
+      service.inputService.disable();
+      service.stimulusService.hide();
+      service.endTime = timing;
+      service.processResponse(input.givenResponse);
+    };
+
+  }]);
---
+++ app/projects/public/Representation Assymetry/executables/chimeric.service.js
@@ -0,0 +1,69 @@
+tatool.factory('chimeric',['executableUtils', 'timerUtils', 'stimulusServiceFactory', 'inputServiceFactory', 'dbUtils',
+  function (executableUtils, timerUtils, stimulusServiceFactory, inputServiceFactory, dbUtils) {
+
+    var chimeric = executableUtils.createExecutable();
+
+    var DISPLAY_DURATION_DEFAULT = 2000;
+
+    chimeric.prototype.init = function() {
+      var promise = executableUtils.createPromise();
+
+      this.counter = 0;
+      this.stimulusService = stimulusServiceFactory.createService(this.stimuliPath);
+      this.inputService = inputServiceFactory.createService(this.stimuliPath);
+
+      //The stimuli list is a CSV explained in Tatool UI
+      var self = this;
+      executableUtils.getCSVResource(this.stimuliFile, true, this.stimuliPath).then(
+        function(list) {
+          self.stimuliList = executableUtils.shuffle(list);
+          self.setupInputKeys(list);
+          promise.resolve();
+        }, function(error) {
+          promise.reject(error);
+        });
+
+      //Create a timer object in the Executable service init method
+      this.displayDuration = (this.displayDuration ) ? this.displayDuration : DISPLAY_DURATION_DEFAULT;
+      this.timer = timerUtils.createTimer(this.displayDuration, true, this);
+
+      return promise;
+    };
+
+    // Adding keyInputs and show by default
+    chimeric.prototype.setupInputKeys = function(list) {
+      var keys = this.inputService.addInputKeys(list, !this.showKeys.propertyValue);
+
+      if (keys.length === 0) {
+        executableUtils.fail('Error creating input template for Executable tatoolSimon. No keyCode provided in stimuliFile.');
+      }
+    };
+
+    chimeric.prototype.createStimulus = function() {
+      //Get the stimulus
+      var stimulus = executableUtils.getNext(this.stimuliList, this.counter);
+
+      //Keep a record
+      this.trial = {};
+      this.trial.stimulusType = stimulus.stimulusType;
+      this.trial.stimulusValue = stimulus.stimulusValue;
+
+      //Show the image
+      this.stimulusService.setImage(stimulus.stimulusValue);
+
+      this.counter++;
+
+    };
+
+    chimeric.prototype.processResponse = function(response) {
+      this.trial.reactionTime = this.endTime - this.startTime;
+      this.trial.givenResponse = response;
+      dbUtils.saveTrial(this.trial).then(executableUtils.stop);
+    };
+
+    chimeric.prototype.stopExecution = function() {
+      executableUtils.stop();
+    };
+
+    return chimeric;
+  }]);
