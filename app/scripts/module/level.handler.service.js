ca7d6f750bcc1d316844c238c652e01e30ccef66
cvonbastian <claudia@outerlimits.ch>
2020-08-08 14:18:58 +0100

    Extended level handler
    Extended to enable setting a benchmark range (upper benchmark for level up, lower benchmark for level down, anything in-between keeps difficulty the same) and to adjust performance based on median RTs.

--- app/scripts/module/level.handler.service.js
+++ app/scripts/module/level.handler.service.js
@@ -15,7 +15,21 @@ function LevelHandlerService($log, $rootScope, statusPanelService, handlerServic
       // preset properties that can be overwritten by properties in module file
       this.benchmarkSampleSize = 3;
       this.benchmark = 0.6;
-      this.allowLevelDown = { propertyType: 'Boolean', propertyValue: false};
+      this.allowLevelDown = { propertyType: 'Boolean', propertyValue: false };
+
+      // enable setting a range where difficulty is maintained 
+      this.setBenchmarkRange = { propertyType: 'Boolean', propertyValue: false }; 
+      this.lowerBenchmark = 0.4;
+
+      // settings for handling of reaction times
+      this.enableReactionTimeBenchmark = { propertyType: 'Boolean', propertyValue: false };
+      this.reactionTimeList = [];
+      this.medianReactionTime = 0;
+      this.reactionTimeBenchmark = 5000;
+
+      // enable setting a range where difficulty is maintained 
+      this.setReactionTimeBenchmarkRange = { propertyType: 'Boolean', propertyValue: false }; 
+      this.lowerReactionTimeBenchmark = 1500;
     };
 
     // listens to phase changes and triggers the handler
@@ -26,6 +40,7 @@ function LevelHandlerService($log, $rootScope, statusPanelService, handlerServic
         this.totalScore = (moduleService.getModuleProperty(this, 'totalScore') !== undefined) ? moduleService.getModuleProperty(this, 'totalScore') : 0;
         this.maxScore = (moduleService.getModuleProperty(this, 'maxScore') !== undefined) ? moduleService.getModuleProperty(this, 'maxScore') : 0;
         this.currentLevel = (moduleService.getModuleProperty(this, 'currentLevel') !== undefined) ? moduleService.getModuleProperty(this, 'currentLevel') : 1;
+
       } else if (phase === tatoolPhase.EXECUTABLE_START) {
         this.updateStatusPanel();
       } else if (phase === tatoolPhase.EXECUTABLE_END) {
@@ -44,6 +59,10 @@ function LevelHandlerService($log, $rootScope, statusPanelService, handlerServic
           this.totalScore += currentTrial.score;
           this.maxScore++;
         }
+
+        if ('reactionTime' in currentTrial) {
+          this.reactionTimeList.push(currentTrial.reactionTime);
+        }
       }
 
       // if all child elements are completed (especially dual elements), process level
@@ -51,20 +70,95 @@ function LevelHandlerService($log, $rootScope, statusPanelService, handlerServic
         this.counter++;
         if (this.counter >= this.benchmarkSampleSize) {
           var performance = this.totalScore / this.maxScore;
+
+          // compute median reaction time
+          this.medianReactionTime = median(this.reactionTimeList);
+
+          // create variables to check performance & reaction times first to simplify following code
+          var passedUpperScoreBenchmark = false;
+          var failedLowerScoreBenchmark = false;
+          var passedUpperReactionTimeBenchmark = false;
+          var failedLowerReactionTimeBenchmark = false;
+
+          var levelUp = false;
+          var levelDown = false; 
+
+          // check whether score exceeds upper benchmark
           if (performance >= this.benchmark) {
+            passedUpperScoreBenchmark = true;
+          } 
+
+          // check whether score is below lower benchmark
+          if (this.setBenchmarkRange.propertyValue) {
+            if (performance <= this.lowerBenchmark) {
+              failedLowerScoreBenchmark = true;
+            }
+          }
+
+          // for reaction times, smaller values reflect "better" performance
+          if (this.medianReactionTime <= this.reactionTimeBenchmark) {
+            passedUpperReactionTimeBenchmark = true;
+          } 
+
+          if (this.setReactionTimeBenchmarkRange.propertyValue) {
+            if (this.medianReactionTime >= this.lowerReactionTimeBenchmark) {
+              failedLowerReactionTimeBenchmark = true;
+            }
+          }
+
+          // adjust level based on scores and reaction times
+          if (this.enableReactionTimeBenchmark.propertyValue) {
+              if (passedUpperScoreBenchmark && passedUpperReactionTimeBenchmark) {
+                levelUp = true;
+
+                // legacy routine for adjusting downward without benchmark range
+              } else if (this.allowLevelDown.propertyValue && this.currentLevel > 1) {
+                if (!this.setBenchmarkRange.propertyValue && !this.setReactionTimeBenchmarkRange.propertyValue) {
+                  levelDown = true;
+
+                  // adjust based on lower benchmarks
+                } else if ((this.setBenchmarkRange.propertyValue && failedLowerScoreBenchmark) ||
+                  (this.setReactionTimeBenchmarkRange.propertyValue && failedLowerReactionTimeBenchmark)) {
+                  levelDown = true;
+                }
+              }
+
+          // adjust level based on scores only
+          } else {
+            if (passedUpperScoreBenchmark) {
+              levelUp = true;
+            } else if (this.allowLevelDown.propertyValue && this.currentLevel > 1) {
+
+              // legacy routine for adjusting downward without benchmark range
+              if (!this.setBenchmarkRange.propertyValue) {
+                levelDown = true;
+
+                // adjust based on lower benchmark
+              } else if (failedLowerScoreBenchmark) {
+                levelDown = true;
+              }
+            }
+          }
+
+          if (levelUp) {
             this.currentLevel++;
             $rootScope.$broadcast(tatoolPhase.LEVEL_CHANGE, this.currentLevel);
-          } else if (this.allowLevelDown.propertyValue && this.currentLevel > 1) {
+          } else if (levelDown) {
             this.currentLevel--;
             $rootScope.$broadcast(tatoolPhase.LEVEL_CHANGE, this.currentLevel);
           }
+
           this.counter = 0;
           this.totalScore = 0;
           this.maxScore = 0;
+
+          // reset reactionTime variables
+          this.reactionTimeList = [];
+          this.medianReactionTime = 0;
         }
       }
 
-      // save current property values to module
+      // save current property values to module (note that RTs cannot be carried over across sessions)
       moduleService.setModuleProperty(this, 'counter', this.counter);
       moduleService.setModuleProperty(this, 'totalScore', this.totalScore);
       moduleService.setModuleProperty(this, 'maxScore', this.totalScore);
@@ -79,6 +173,24 @@ function LevelHandlerService($log, $rootScope, statusPanelService, handlerServic
     // updates the status panel
     LevelHandler.prototype.updateStatusPanel = function() {
       statusPanelService.updateLevel(this.currentLevel);
+    };
+
+    // helper function to compute the median
+    function median(numbers) {
+      var median = 0;
+      var numsLen = numbers.length;
+
+      numbers.sort(function(a, b){return a - b});
+
+      if (numsLen % 2 === 0) { // is even
+        // average of two middle numbers
+        median = (numbers[numsLen / 2 - 1] + numbers[numsLen / 2]) / 2;
+      } else { // is odd
+        // middle number only
+        median = numbers[(numsLen - 1) / 2];
+      }
+ 
+      return median;
     };
 
     return LevelHandler;    
