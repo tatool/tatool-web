b52e95ac5f05e096088cb3f9277479b1c689bffd
cvonbastian <claudia@outerlimits.ch>
2022-01-08 14:57:47 +0000

    tatoolSurvey
    Example survey created with SurveyJS
    https://surveyjs.io/create-survey

---
+++ app/projects/public/tatool-share/executables/tatoolSurvey.ctrl.js
@@ -0,0 +1,28 @@
+'use strict';
+
+tatool
+  .controller('tatoolSurveyCtrl', ['$scope', 'service', 'moduleService',
+    function ($scope, service, moduleService) {
+
+    var myCss = {
+   		footer: "",
+   		question: {
+			"title": "surveyquestion"
+		},
+		boolean: {
+			"label": "surveyquestion",
+		},
+	};
+
+    $scope.start = function() {
+    	var survey = new service.Survey.Model(service.surveyJSON);
+
+        $("#surveyContainer").Survey({
+        	model: survey,
+        	onComplete: service.saveSurvey,
+        	css: myCss
+        });
+    };
+
+
+  }]);
